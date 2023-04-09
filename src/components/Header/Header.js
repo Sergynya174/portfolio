4b5c3eb4383f4369b97a3de785af78b37fa29748
Sergynya174 <Sergynya174@yandex.ru>
2023-04-09 23:54:04 +0300

    доработки

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,24 @@
 import { useState } from "react";
 import useWindowDimensions from "../../utils/useWindowDimensios";
 import styles from "./Header.module.css";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { width } = useWindowDimensions();
+  const location = useLocation();
 
   const clickOpenNavbar = () => {
     setIsOpen(!isOpen);
   };
+
+  console.log(location);
+
+  const home = location.hash === "#home";
+  const about = location.hash === "#about_me";
+  const skills = location.hash === "#skills";
+  const portfolio = location.hash === "#portfolio";
+  const contacts = location.hash === "#contacts";
 
   return (
     <header className={styles.header}>
@@ -24,19 +34,31 @@ const Header = () => {
               id={styles.menu}
             ></i>
             <nav className={styles.navbar}>
-              <a className={styles.link} href="#home">
+              <a className={home ? styles.linkColor : styles.link} href="#home">
                 Home
               </a>
-              <a className={styles.link} href="#about_me">
+              <a
+                className={about ? styles.linkColor : styles.link}
+                href="#about_me"
+              >
                 About
               </a>
-              <a className={styles.link} href="#skills">
+              <a
+                className={skills ? styles.linkColor : styles.link}
+                href="#skills"
+              >
                 Skills
               </a>
-              <a className={styles.link} href="#portfolio">
+              <a
+                className={portfolio ? styles.linkColor : styles.link}
+                href="#portfolio"
+              >
                 Portfolio
               </a>
-              <a className={styles.link} href="#contacts">
+              <a
+                className={contacts ? styles.linkColor : styles.link}
+                href="#contacts"
+              >
                 Contact
               </a>
             </nav>
@@ -50,19 +72,28 @@ const Header = () => {
         )
       ) : (
         <nav className={styles.navbar}>
-          <a className={styles.link} href="#home">
+          <a className={home ? styles.linkColor : styles.link} href="#home">
             Home
           </a>
-          <a className={styles.link} href="#about_me">
+          <a
+            className={about ? styles.linkColor : styles.link}
+            href="#about_me"
+          >
             About
           </a>
-          <a className={styles.link} href="#skills">
+          <a className={skills ? styles.linkColor : styles.link} href="#skills">
             Skills
           </a>
-          <a className={styles.link} href="#portfolio">
+          <a
+            className={portfolio ? styles.linkColor : styles.link}
+            href="#portfolio"
+          >
             Portfolio
           </a>
-          <a className={styles.link} href="#contacts">
+          <a
+            className={contacts ? styles.linkColor : styles.link}
+            href="#contacts"
+          >
             Contact
           </a>
         </nav>
